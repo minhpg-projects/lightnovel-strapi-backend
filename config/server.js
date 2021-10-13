efcc5eb70c6edd481bbde7c2168fea23368cff15
minhpg <67231681+minhpg@users.noreply.github.com>
2021-10-13 15:46:04 +0700

    init

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '9ee16a0fde6d8ffcdda805839bdd975e'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cbdb5b6e-3cb1-41e4-981d-d43d25e4eb3f'
+};
